c1713aa7bb4a64abf222f0606f0cc12168b927fe
Leonid Gadetsky <leonidgadetsky@yandex.ru>
2021-10-25 12:49:14 +0300

    Initial commit

---
+++ src/main/java/ru/dataart/academy/java/Main.java
@@ -0,0 +1,17 @@
+package ru.dataart.academy.java;
+
+public class Main {
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        System.out.println("Reverse integer works: " + reverseInteger.reverse(-456));
+
+        try {
+            System.out.println("Reverse integer works for big negative value: " + reverseInteger.reverse(-2147483648));
+            System.out.println("Reverse integer works for big positive value: " + reverseInteger.reverse(2147483647));
+        }
+        catch (RuntimeException exc) {
+            System.out.println(exc.getMessage());
+        }
+
+    }
+}
---
+++ src/main/java/ru/dataart/academy/java/ReverseInteger.java
@@ -0,0 +1,32 @@
+package ru.dataart.academy.java;
+
+public class ReverseInteger {
+    /**
+     * @param inputNumber - any integer value
+     * @return - inputNumber in reversed order
+     * Example: 1234 -> 4321
+     * -23 -> -32
+     * 120 -> 21
+     * -2,147,483,648 -> exception and error message
+     * If result (number in reversed order) is out of int range [-2,147,483,648,  2,147,483,647] throw custom
+     * unchecked exception. In main method (Main class) if exception happened output message with problem to user.
+     */
+    public int reverse(int inputNumber) {
+        int reverseNumber = 0;
+
+        while (inputNumber != 0) {
+            if (intRange(((long)reverseNumber * 10 + inputNumber % 10))) {
+                reverseNumber = reverseNumber * 10 + inputNumber % 10;
+                inputNumber /= 10;
+            }
+            else {
+                throw new RuntimeException("This number can't be reversed!");
+            }
+        }
+        return reverseNumber;
+    }
+
+    public boolean intRange(long number) {
+        return (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE);
+    }
+}
